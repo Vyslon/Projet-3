fae5b41b0e9467619f0297418a45be325ae6d83b
Thomas SANTONI <santoni.thomas26@hotmail.fr>
2018-02-01 19:05:08 +0100

    Add several methods to class Level (__init__, generator & display)

--- Classes.py
+++ Classes.py
@@ -1,9 +1,19 @@
 import Main
 
 class Level:
-    pass
+    def __init__(self):
+        #initialize attributes
+        pass
 
+    def generator(self, file):
+        #Generate structure
+        #Place objects has random locations
+        pass
 
+    def display(self, window):
+        #Display labyrinth
+        #Display items
+        pass
 
 class Character:
     pass
