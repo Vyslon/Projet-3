69e2b20a8cce881d821b24e5246058c9987c884e
Thomas SANTONI <santoni.thomas26@hotmail.fr>
2018-02-05 14:29:27 +0100

    Adding 1 image, fixed display method, filled menu_loop and game_loop. Displaying menu and labyrinth are working both

--- Classes.py
+++ Classes.py
@@ -19,28 +19,32 @@ class Level:
                 for character in line:
                     if character != '\n':
                         line_level.append(character)
+                        print(character)
 
                 level_structure.append(line_level)
             self.structure = level_structure
 
+        #for i in range (0,3)
+
     def display(self, window):
         #Display items
-        startingpointimage = pygame.image.load("start.png")
-        endingpointimage = pygame.image.load("end.png")
-        wallimage = pygame.image.load("wall.png")
+        startingpointimage = pygame.image.load("start.png").convert()
+        endingpointimage = pygame.image.load("end.png").convert_alpha()
+        wallimage = pygame.image.load("wall.png").convert()
         y, x = 0, 0
+
         for line in self.structure:
+            x = 0
             for character in line:
                 if character == 's':
                     window.blit(startingpointimage, (x * 30, y * 30))
                 if character == 'w':
-                    window.blit(endingpointimage,(x * 30,y * 30))
+                    window.blit(wallimage,(x * 30,y * 30))
                 if character == 'e':
-                    window.blit(wallimage, (x * 30,y * 30))
+                    window.blit(endingpointimage, (x * 30,y * 30))
                 x += 1
             y += 1
 
-        pass
 
 class Character:
     def __init__(self):
--- Main.py
+++ Main.py
@@ -4,17 +4,41 @@ from Constants import *
 from Classes import *
 
 program_loop = 1
-menu_loop = 0
-game_loop = 1
+menu_loop = 1
+game_loop = 0
+
+pygame.init()
 
 window_size = 450
 window = pygame.display.set_mode((window_size, window_size))
+
+"""Display Icon"""
+icon = pygame.image.load("icon.jpg")
+pygame.display.set_icon(icon)
+
+"""Display Title"""
+pygame.display.set_caption("MacGyver Labyrinth")
 
 while program_loop:
     #Display menu
     #Refresh screen
     while menu_loop:
-        pass
+        pygame.time.Clock().tick(30)
+
+        background = pygame.image.load("menu_background.png").convert()
+        window.blit(background, (0, 0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                menu_loop = 0
+                program_loop = 0
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN | K_KP_ENTER:
+                    menu_loop = 0
+                    game_loop = 1
+
+
     #Limit the number of frames per second so that the program will not take too much ressources
     #For every event
     #   If quit icon
@@ -32,10 +56,24 @@ while program_loop:
     while game_loop:
         pygame.time.Clock().tick(30)
 
-        background = pygame.image.load("background.png").convert()
+        background = pygame.image.load("background.jpg").convert()
         window.blit(background, (0, 0))
-        game_loop = 0
-        program_loop = 0
+        pygame.display.flip()
+
+        lvl = Level("level_config.txt")
+        lvl.generator()
+        lvl.display(window)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                game_loop = 0
+                program_loop = 0
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    game_loop = 0
+                    menu_loop = 1
+
+
     #For every event
     #   If quit icon
     #       game_loop = 0
