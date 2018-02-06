e4fdd256254e100b78bda9056ebc45f076ea7705
Thomas SANTONI <santoni.thomas26@hotmail.fr>
2018-02-06 11:35:26 +0100

    Adding statements for x and y in generator method, BE CAREFUL : the starting point should always be at (0,0)

--- Classes.py
+++ Classes.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from pygame.locals import *
 
@@ -8,7 +9,7 @@ class Level:
         """
         self.lvlConfigFile = file
         self.structure = 0
-
+        self.items = 0
 
     def generator(self):
         """Generate structure and place items has random locations"""
@@ -24,13 +25,23 @@ class Level:
                 level_structure.append(line_level)
             self.structure = level_structure
 
-        #for i in range (0,3)
+        for i in range (0,3):
+            x, y = 0, 0
+            while (self.structure[y][x] != 'n') & (y, x) != (self.items[0] | self.items[1] | self.items[2]):
+                y = random.randrange(0, len(self.structure)-1, 1)
+                x = random.randrange(0, len(self.structure)-1, 1)
+            self.items[i] = (y,x)
+
+
 
     def display(self, window):
         #Display items
         startingpointimage = pygame.image.load("start.png").convert()
         endingpointimage = pygame.image.load("end.png").convert_alpha()
         wallimage = pygame.image.load("wall.png").convert()
+        item1 = pygame.image.load("item1.png").convert_alpha()
+        item2 = pygame.image.load("item2.png").convert_alpha()
+        item3 = pygame.image.load("item3.png").convert_alpha()
         y, x = 0, 0
 
         for line in self.structure:
@@ -42,6 +53,12 @@ class Level:
                     window.blit(wallimage,(x * 30,y * 30))
                 if character == 'e':
                     window.blit(endingpointimage, (x * 30,y * 30))
+                if (y, x) == self.items[0]:
+                    window.blit(item1, (x * 30, y * 30))
+                elif (y, x) == self.items[1]:
+                    window.blit(item2, (x * 30, y * 30))
+                elif (y, x) == self.items[2]:
+                    window.blit(item3, (x * 30, y * 30))
                 x += 1
             y += 1
 
